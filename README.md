5134f566b979910a9ea3a4ec65f7a70ea6d36e68
Aric Huang <arichuang@gmail.com>
2020-08-27 11:32:18 -0700

    tweaks

---
+++ README.md
@@ -0,0 +1,9 @@
+# Surface Throttle Test
+
+## Env vars
+```
+CPUS=0							# how many CPUs to stress in stress-ng. 0 means all CPUs
+CPU_PERCENT=100					# how much CPU to use in stress-ng
+THROTTLE_THRESHOLD=800000		# threshold to report throttle events in stdout
+DEBUG=false						# set to true to log freq on every tick
+```
--- start.sh
+++ start.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-echo "Cores: $CORES"
+echo "CPUs: $CPUS"
 echo "CPU stress %: $CPU_PERCENT"
 echo "Throttle threshold: $THROTTLE_THRESHOLD"
 
-stress-ng -c $CORES -l $CPU_PERCENT &
+stress-ng -c $CPUS -l $CPU_PERCENT &
 
 while true;
 do
 	currentFreq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
 	if [ "$DEBUG" == "true" ]; then
+		echo "==================== Tick ====================="
 		echo $currentFreq
+		sensors
+		echo "==============================================="
 	fi
 	if [ "$currentFreq" -lt "$THROTTLE_THRESHOLD" ]; then
 		echo "==================== Throttle detected ====================="
